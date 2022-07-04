57d70a01a0324f55921a5278ee4456afc32deb1c
unknown <nikmanesh@gmail.com>
2022-07-04 22:17:11 +0430

    media care

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/* adding acrive class for navbar */
+var header = document.getElementById("navbarNav");
+var btns = header.getElementsByClassName("nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+/* end adding acrive class for navbar */
+
+
